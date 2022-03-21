2409ebb4abd87fc930778f50e4b193edc598a875
Patrick Curran <patrickcurran48@hotmail.com>
2022-03-21 19:50:16 +0000

    account verification , individual accounts now working

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -3,9 +3,11 @@ import { db } from "../models/db.js";
 export const dashboardController = {
   index: {
     handler: async function (request, h) {
-      const placemarks = await db.placemarkStore.getAllPlacemarks();
+      const loggedInUser = request.auth.credentials;
+      const placemarks = await db.placemarkStore.getAllPlacemarks(loggedInUser._id);
       const viewData = {
         title: "Placemarks Dashboard",
+        user: loggedInUser,
         placemarks: placemarks,
       };
       return h.view("dashboard-view", viewData);
@@ -14,7 +16,9 @@ export const dashboardController = {
 
   addPlacemark: {
     handler: async function (request, h) {
+      const loggedInUser = request.auth.credentials;
       const newPlacemark = {
+        userid: loggedInUser._id,
         title: request.payload.title,
       };
       await db.placemarkStore.addPlacemark(newPlacemark);
